d268a62fb57def56b8b0ebbbc63e41962d8801f0
Maria Vitória J. <oliveirawilliam472@gmail.com>
2024-11-14 17:44:48 -0300

    atividade1

---
+++ antecessor e sucessor/soma.js
@@ -0,0 +1,7 @@
+// antecessor e sucessor 
+var n1 = prompt("digite um numero")
+
+var antecessor = Number(n1) - 1 
+var sucessor = Number(n1) + 1 
+alert('o antecessor é:'+antecessor+'')
+alert('o sucessor é:'+sucessor+'')

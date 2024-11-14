fb3a99448edc4efcffa850bc5d4672d66b297d88
Maria Vitória J. <oliveirawilliam472@gmail.com>
2024-11-14 17:25:39 -0300

    calculadora

---
+++ atividade 1 java/soma.js
@@ -0,0 +1,9 @@
+//calculadora  para mesa de jantar
+
+var n1 = prompt("digite o numero de pessoas a mesa") 
+var n2 = prompt("digite o valor da conta")
+
+var divide = Number(n2) /  Number(n1)
+
+alert('o valor a ser pago é:  '+divide+' ');
+
--- javascript/somar.js
+++ javascript/somar.js
@@ -4,3 +4,5 @@ var n2 = prompt("digite o segundo numero")
 var soma = Number(n1) + Number(n2)
 
 alert('somados dois numero deu:'+soma)
+
+var variavel_global = 1 

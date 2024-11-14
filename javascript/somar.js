5331f84ddbd6fb54eccbc55cb582c724482ae5f5
Maria Vitória J. <oliveirawilliam472@gmail.com>
2024-11-14 16:28:37 -0300

    javascript

---
+++ javascript/somar.js
@@ -0,0 +1,6 @@
+var n1 = prompt("digite um numero")
+var n2 = prompt("digite o segundo numero")
+ 
+var soma = Number(n1) + Number(n2)
+
+alert('somados dois numero deu:'+soma)
---
+++ javascript/teste.js
@@ -0,0 +1,18 @@
+// caixa de texto
+// alert("helloworld")
+// var nome = prompt("qual seu nome")
+// confirm("deseja baixar novamente")
+
+// variaveis
+// string - caractere
+// number - numero (real , inteiro)
+// boolean - logico (verdadeiro ou falso)
+// array - vetor(lista) 
+var nome = prompt('qual seu nome')
+var numero_fav = 77
+var ligado = true
+var valor_total = 1.7
+var lista = []
+  
+var aniversario = prompt('qual e o dia do seu aniversario?')
+alert('ola '+nome+' o dia do seu aniversaio é '+aniversario+'')

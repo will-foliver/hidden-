6fe6a6f264ee7ebb063c525273c87da4a7b78f5d
Maria Vitória J. <oliveirawilliam472@gmail.com>
2024-11-18 17:43:56 -0300

    top
    vamo que vamo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-function explicaçao1(){
+function excercicio1(){
  escolha = confirm("deseja rodar true or false")
 if (escolha){
 alert("vc escolheu true")
@@ -7,7 +7,7 @@ alert("vc escolheu true")
 }
 }
 
-function explicaçao2(){
+function excecicio2(){
 escolha = prompt(" a) bolo no pote: R$10, b) açai R$15 c) torta R$15")  
 switch(escolha){
 case 'a':
@@ -23,13 +23,10 @@ break
 } 
 }
 
-function explicaçao3(){
-idade = Number(prompt('digite sua idade'))
-if(idade >= 18){ 
-alert('seja bem vindo')
-document.querySelector = ("body").innerhtml = "vivaaaaaa"
+function excercicio3(){
+nome = Nota (prompt('digite sua nota'))
+if(nota >= 7 ){
 }else{
-alert('idade invalida')
-window.location = 'https://www.google.com'
+alert('voce esta reprovado')
 }
 }

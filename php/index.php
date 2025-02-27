a8c77f94a717375f56ce402eec629dd09474935c
mariavitoriajesus <102930406+mariavitoriajesus@users.noreply.github.com>
2025-02-27 16:53:28 -0300

    php introducao

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php atividade</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <?php  
+    echo "malik esteve aqui <br>";
+    echo "spice and the eyes of the others is juice <br>";
+    echo "the eyes of the others is juice <br>";
+    $palavra = "bolo";
+    echo date('d/m/y ');
+    phpinfo();
+    ?>
+</body>
+</html>
